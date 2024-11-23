bc01a1522846824eb1faf1647aa6fc265ab0695d
Ilya Fedorov <74106915+MrPelmeshik@users.noreply.github.com>
2024-11-23 19:51:08 +0300

    feat: Пустой экран по умолчанию

--- src/app-front/src/App.tsx
+++ src/app-front/src/App.tsx
@@ -3,12 +3,11 @@ import css from './App.module.css';
 import {MenuComponent} from "./components/Menu/MenuComponent";
 import {Grid, GridItem} from "@consta/uikit/Grid";
 import {Theme, presetGpnDefault} from "@consta/uikit/Theme";
-import {TestBlockComponent} from "./components/TestBlock/TestBlockComponent";
 import {Footer} from "./components/Footer/FooterComponent";
 import {HeaderComponent} from "./components/Header/HeaderComponent";
 
 export const App = () => {
-    const [mainBlock, setMainBlock] = useState<JSX.Element>(<TestBlockComponent />);
+    const [mainBlock, setMainBlock] = useState<JSX.Element>(<></>);
 
     return <Theme preset={presetGpnDefault}>
         <Grid cols={10}
